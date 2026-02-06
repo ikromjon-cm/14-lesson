0a7060ef84446339875cb6a41ab58732861782e2
user <akparaliyevikromjon7@gmail.com>
2026-02-06 15:15:08 +0500

    added useEffect()

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,22 @@ import { useEffect, useState } from "react";
 const App = () => {
   const [data,setData] = useState([])
 
-  useEffect(()=>
+  useEffect(()=>{
   fetch("https://dummyjson.com/products")
   .then((res)=>res.json())
-  )
+  .then((data)=> setData(data.products))
+},[])
+  
   return (
     <div>
+      {data.map((item,index)=>
+      <div className="container" key={item.id}> 
+      <img src={item.images[0]} alt={item.title} />
+      <h4>{item.title}</h4>
+      <p>{item.discription}</p>
       
+      </div>
+      )}
     </div>
   );
 }
